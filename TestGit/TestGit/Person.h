01d13176a5f0c3c4caf7360933f70ecee4c5bcc4
skyA883 <shuxiaxihongshi@163.com>
2014-03-16 18:56:15 +0800

    创建person类

---
+++ TestGit/TestGit/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestGit
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestGit/TestGit/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestGit
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
