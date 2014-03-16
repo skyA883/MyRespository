fc2eaa7a8809aee328a0c869c4874d6c9875a88b
skyA883 <shuxiaxihongshi@163.com>
2014-03-16 19:36:15 +0800

    创建student类

---
+++ TestGit/TestGit/Sutdent.h
@@ -0,0 +1,13 @@
+//
+//  Sutdent.h
+//  TestGit
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sutdent : NSObject
+
+@end
---
+++ TestGit/TestGit/Sutdent.m
@@ -0,0 +1,13 @@
+//
+//  Sutdent.m
+//  TestGit
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import "Sutdent.h"
+
+@implementation Sutdent
+
+@end
