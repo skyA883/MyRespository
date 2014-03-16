06c42bade0bc2cffaf6bf5ca8717b38ffef5d33f
skyA883 <shuxiaxihongshi@163.com>
2014-03-16 19:34:34 +0800

    ‘Test’的初始提交

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 璇 刘 on 14-3-16.
+//  Copyright (c) 2014年 璇 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
